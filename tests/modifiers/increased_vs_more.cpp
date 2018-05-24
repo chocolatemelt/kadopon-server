bbc64c1b52dbf1ff5c935df7f160890aa344cefb
Kevin Zhang <kzhaaang@gmail.com>
2018-05-24 19:41:29 -0400

    #41 | supplementary tests

---
+++ tests/modifiers/increased_vs_more.cpp
@@ -0,0 +1,23 @@
+/**
+ * tests/modifiers/increased_vs_more.cpp
+ * Tests standard positive scaling modifier interactions with components.
+ * "increased" modifiers are an additive scale while "more" modifiers are multiplicative.
+ */
+#include "catch.hpp"
+#include "components/Life.hpp"
+#include "entities/Character.hpp"
+#include "entityx/entityx.h"
+#include "systems/ScalingModifiers.hpp"
+
+namespace ex = entityx;
+
+TEST_CASE("increased life scales appropriately", "[modifiers][character]") {
+	ex::Entity e;
+	ex::EventManager events;
+	ex::EntityManager manager(events);
+	e = manager.create();
+
+	Character c1("ranger", e);
+	e.assign<Life>(100);
+	CHECK(100 == e.component<Life>()->life);
+}
