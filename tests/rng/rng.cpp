bf19fa44fc759409e654a5f66a14deb935b8a595
Kevin Zhang <kzhaaang@gmail.com>
2018-05-24 19:44:21 -0400

    #41 | hide rng test

--- tests/rng/rng.cpp
+++ tests/rng/rng.cpp
@@ -11,8 +11,8 @@
 
 TEST_CASE("RNG distribution hits 0-99", "[rng]") {
 	// switched to mt19937 for mac compilation... high sierra has a problem with xorshift apparently
-	std::mt19937 eng;
-	std::uniform_int_distribution<int> dist(0, 99);
-	for (int i = 0; i < 1000; ++i) fmt::print("rng: {}\n", dist(eng));
-	CHECK(1==1);
+	// std::mt19937 eng;
+	// std::uniform_int_distribution<int> dist(0, 99);
+	// for (int i = 0; i < 1000; ++i) fmt::print("rng: {}\n", dist(eng));
+	// CHECK(1==1);
 }
