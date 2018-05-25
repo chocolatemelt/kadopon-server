05250b34e3b13852a39f9c59d12d80a188eb6942
Kevin Zhang <kzhaaang@gmail.com>
2018-05-24 21:22:23 -0400

    #41 | failing test

--- tests/modifiers/increased_vs_more.cpp
+++ tests/modifiers/increased_vs_more.cpp
@@ -3,21 +3,37 @@
  * Tests standard positive scaling modifier interactions with components.
  * "increased" modifiers are an additive scale while "more" modifiers are multiplicative.
  */
+#include <vector>
+
 #include "catch.hpp"
 #include "components/Life.hpp"
 #include "entities/Character.hpp"
 #include "entityx/entityx.h"
-#include "systems/ScalingModifiers.hpp"
+#include "systems/ScalingModifierSystem.hpp"
 
 namespace ex = entityx;
 
-TEST_CASE("increased life scales appropriately", "[modifiers][character]") {
-	ex::Entity e;
-	ex::EventManager events;
-	ex::EntityManager manager(events);
-	e = manager.create();
+// EntityX ??? class
+class EntitiesFixture : public ex::EntityX {
+ public:
+  std::vector<ex::Entity> created_entities;
 
-	Character c1("ranger", e);
-	e.assign<Life>(100);
-	CHECK(100 == e.component<Life>()->life);
+  EntitiesFixture() {
+		ex::Entity e;
+		created_entities.push_back(e);
+		Character c1("ranger", e);
+		e.assign<Life>(100);
+  }
+};
+
+TEST_CASE_METHOD(EntitiesFixture, "increased life scales appropriately", "[modifiers][character]") {
+	systems.add<ScalingModifierSystem>();
+	systems.configure();
+
+  systems.update<ScalingModifierSystem>(0);
+	ex::ComponentHandle<Life> life;
+	for(auto e : created_entities) {
+		e.unpack<Life>(life);
+		CHECK(110 == life->life);
+	}
 }
