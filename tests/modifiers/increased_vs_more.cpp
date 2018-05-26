546bc85b99f4b9649af0ffb85180e33e80d29bac
Kevin Zhang <kzhaaang@gmail.com>
2018-05-26 18:48:06 -0400

    #59 | updated test case

--- tests/modifiers/increased_vs_more.cpp
+++ tests/modifiers/increased_vs_more.cpp
@@ -10,6 +10,7 @@
 #include "components/Mana.hpp"
 #include "entities/Character.hpp"
 #include "entityx/entityx.h"
+#include "modifiers/IncreasedModifier.hpp"
 #include "systems/ScalingModifierSystem.hpp"
 
 namespace ex = entityx;
@@ -19,13 +20,14 @@ TEST_CASE("increased life scales appropriately", "[modifiers][character]") {
   ex::EntityManager entities(events);
   ex::SystemManager systems(entities, events);
 
-  ex::Entity e;
+  Character c1("ranger");
+  ex::Entity e = (ex::Entity) c1;
   e = entities.create();
+
 
   systems.add<ScalingModifierSystem>();
   systems.configure();
 
-  Character c1("ranger", e);
   e.assign<Life>(100);
   CHECK(100 == e.component<Life>()->total);
   systems.update<ScalingModifierSystem>(0.0);
@@ -35,4 +37,7 @@ TEST_CASE("increased life scales appropriately", "[modifiers][character]") {
   CHECK(100 == e.component<Mana>()->total);
   systems.update<ScalingModifierSystem>(0.0);
   CHECK(110 == e.component<Mana>()->total);
+
+  // IncreasedModifier<int> incMaxLife("MAXIMUM_LIFE", 10);
+  // c1.testAddModifier(incMaxLife);
 }
