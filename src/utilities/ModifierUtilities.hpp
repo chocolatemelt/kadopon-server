884214fbbb964a797509ca37abc1576b32a490f1
Kevin Zhang <kzhaaang@gmail.com>
2018-05-27 15:21:51 -0400

    #64 | iterate over map to calculate multiplicative result

--- src/utilities/ModifierUtilities.hpp
+++ src/utilities/ModifierUtilities.hpp
@@ -25,6 +25,23 @@ public:
 				handle->additive += val;
 			}
 		}
+		else if(uid == "MORE_MAX_LIFE") {
+			if(std::is_base_of<ScalingComponent, M>::value) {
+				if(handle->multipliers.find(val) != handle->multipliers.end()) ++handle->multipliers[val];
+				else handle->multipliers[val] = 1;
+			}
+		}
+	}
+
+	template <typename M>
+	static void update_total(entityx::ComponentHandle<M> handle) {
+		if(std::is_base_of<ScalingComponent, M>::value) {
+			handle->total = (handle->base + handle->flat) * (1 + (handle->additive / 100.0));
+			// apply more multipliers
+			for(auto multi : handle->multipliers) {
+				for(int i = 0; i < multi.second; ++i) handle->total *= 1 + (multi.first / 100.0);
+			}
+		}
 	}
 
 private:
