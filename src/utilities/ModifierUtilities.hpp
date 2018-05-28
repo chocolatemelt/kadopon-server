9c7279e6e56110cfe40daeed2d29f01c29e075de
Kevin Zhang <kzhaaang@gmail.com>
2018-05-27 22:03:16 -0400

    #69 | revamp utility to calculate total based on given attribute and modifiers

--- src/utilities/ModifierUtilities.hpp
+++ src/utilities/ModifierUtilities.hpp
@@ -5,48 +5,25 @@
 #pragma once
 
 #include <cmath>
-#include <string>
-#include <type_traits>
+// #include <string>
+// #include <type_traits>
 
-#include "components/ScalingComponent.hpp"
 #include "entityx/entityx.h"
 
 class ModifierUtilities {
 public:
-	template <typename M>
-	static void add_flat_mod(entityx::ComponentHandle<M> handle, int val) {
-		if(std::is_base_of<ScalingComponent, M>::value) {
-			handle->flat += val;
-		}
-	}
-
-	template <typename M>
-	static void add_additive_mod(entityx::ComponentHandle<M> handle, int val) {
-		if(std::is_base_of<ScalingComponent, M>::value) {
-			handle->additive += val;
-		}
-	}
-
-	template <typename M>
-	static void add_multiplicative_mod(entityx::ComponentHandle<M> handle, int val) {
-		if(std::is_base_of<ScalingComponent, M>::value) {
-			// multiplicative modifiers are held in a map with their count; simply increment if it exists
-			if(handle->multipliers.find(val) != handle->multipliers.end()) ++handle->multipliers[val];
-			else handle->multipliers[val] = 1;
-		}
-	}
-
-	template <typename M>
-	static void update_total(entityx::ComponentHandle<M> handle) {
-		if(std::is_base_of<ScalingComponent, M>::value) {
-			handle->total = (handle->base + handle->flat) * (1 + (handle->additive / 100.0));
-			// apply more multipliers separately, based on their count
-			for(auto multi : handle->multipliers) {
-				handle->total *= std::pow((1 + (multi.first / 100.0)), multi.second);
-			}
-		}
-	}
+  template <typename Base, typename Add, typename Flat, typename Multi>
+  static void update_total(entityx::ComponentHandle<Base> base,
+                       		 entityx::ComponentHandle<Add> add,
+                     	     entityx::ComponentHandle<Flat> flat,
+                           entityx::ComponentHandle<Multi> multi) {
+    base->maximum = flat->value * (1 + (add->value / 100.0));
+    // apply more multipliers separately, based on their count
+    for(auto multi : multi->multipliers) {
+      base->maximum *= std::pow((1 + (multi.first / 100.0)), multi.second);
+    }
+  }
 
 private:
-	ModifierUtilities() {}
+  ModifierUtilities() {}
 };
