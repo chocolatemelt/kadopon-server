42b395590bea75b0e400dd3d87d09bc050a28fcb
Kevin Zhang <kzhaaang@gmail.com>
2018-06-16 20:18:51 -0400

    add modifierlist component

---
+++ src/components/ModifierList.hpp
@@ -0,0 +1,13 @@
+/**
+ * A list of modifiers attached to an entity. ModifierLists can be applied and removed
+ * from themselves.
+ */
+#pragma once
+
+#include <vector>
+
+#include "modifiers/Modifier.hpp"
+
+struct ModifierList : entityx::Component<ModifierList> {
+  std::vector<Modifier *> modifiers;
+};
