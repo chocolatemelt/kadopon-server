fabc01775be093dab21047bc989f504132157d62
Kevin Zhang <kzhaaang@gmail.com>
2018-07-20 19:01:14 -0400

    #107 | checks

--- tests/instance/instance_creation.cpp
+++ tests/instance/instance_creation.cpp
@@ -3,9 +3,20 @@
  */
 
 #include "catch.hpp"
+#include "components/attributes/Attributes.hpp"
+#include "components/modifiers/Modifiers.hpp"
+#include "entities/Character.hpp"
+#include "entityx/entityx.h"
 #include "instance/GameInstance.hpp"
 
 TEST_CASE("creates a game instance", "[instance]") {
-  GameInstance instance();
-  CHECK(1==1);
+  GameInstance instance;
+
+  Character ranger(instance.entities.create());
+  instance.entities.each<AdditiveLife>([](entityx::Entity entity, AdditiveLife &mana) {
+    CHECK(0 == mana.value);
+  });
+  instance.entities.each<Life>([](entityx::Entity entity, Life &life) {
+    CHECK(0 == life.current);
+  });
 }
