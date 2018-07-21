5f2c663e1d746c737423b44bee4797af4163718a
Kevin Zhang <kzhaaang@gmail.com>
2018-07-21 01:27:13 -0400

    #107 | add flat scaling

--- src/utilities/ItemUtilities.cpp
+++ src/utilities/ItemUtilities.cpp
@@ -9,6 +9,11 @@ void ItemUtilities::equip_item(entityx::Entity character, Item item) {
       entityx::ComponentHandle<FlatLife> handle = character.component<FlatLife>();
       handle->value += mod->value;
     }
+    else if(mod->id() == constants::ADD_LIFE) {
+      // get character handle for flat life
+      entityx::ComponentHandle<AdditiveLife> handle = character.component<AdditiveLife>();
+      handle->value += mod->value;
+    }
     else if(mod->id() == constants::MULTI_LIFE) {
       entityx::ComponentHandle<MultiplicativeLife> handle = character.component<MultiplicativeLife>();
       for(auto v : mod->more) handle->more[v.first] += mod->more[v.first];
