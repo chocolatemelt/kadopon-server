693723b338999b88c85005170fd55ebbcbd79323
Kevin Zhang <kzhaaang@gmail.com>
2018-07-21 00:36:25 -0400

    #107 | use proper constants

--- src/components/modifiers/Modifiers.hpp
+++ src/components/modifiers/Modifiers.hpp
@@ -6,39 +6,40 @@
 #include "components/modifiers/FlatModifier.hpp"
 #include "components/modifiers/AdditiveModifier.hpp"
 #include "components/modifiers/MultiplicativeModifier.hpp"
+#include "misc/Constants.hpp"
 
 /**
  * Life modifiers
  */
 struct FlatLife : FlatModifier {
-  FlatLife() : FlatModifier() { id_ = "FLAT_LIFE"; }
-  FlatLife(int v) : FlatModifier(v) { id_ = "FLAT_LIFE"; }
+  FlatLife() : FlatModifier() { id_ = constants::FLAT_LIFE; }
+  FlatLife(int v) : FlatModifier(v) { id_ = constants::FLAT_LIFE; }
 };
 
 struct AdditiveLife : AdditiveModifier {
-  AdditiveLife() : AdditiveModifier() { id_ = "ADDITIVE_LIFE"; }
-  AdditiveLife(int v) : AdditiveModifier(v) { id_ = "ADDITIVE_LIFE"; }
+  AdditiveLife() : AdditiveModifier() { id_ = constants::ADD_LIFE; }
+  AdditiveLife(int v) : AdditiveModifier(v) { id_ = constants::ADD_LIFE; }
 };
 
 struct MultiplicativeLife : MultiplicativeModifier {
-  MultiplicativeLife() : MultiplicativeModifier() { id_ = "MULTIPLICATIVE_LIFE"; }
-  MultiplicativeLife(int v) : MultiplicativeModifier(v) { id_ = "MULTIPLICATIVE_LIFE"; }
+  MultiplicativeLife() : MultiplicativeModifier() { id_ = constants::MULTI_LIFE; }
+  MultiplicativeLife(int v) : MultiplicativeModifier(v) { id_ = constants::MULTI_LIFE; }
 };
 
 /**
  * Mana modifiers
  */
  struct FlatMana : FlatModifier {
-   FlatMana() : FlatModifier() { id_ = "FLAT_LIFE"; }
-   FlatMana(int v) : FlatModifier(v) { id_ = "FLAT_LIFE"; }
+   FlatMana() : FlatModifier() { id_ = constants::FLAT_MANA; }
+   FlatMana(int v) : FlatModifier(v) { id_ = constants::FLAT_MANA; }
  };
 
  struct AdditiveMana : AdditiveModifier {
-   AdditiveMana() : AdditiveModifier() { id_ = "ADDITIVE_LIFE"; }
-   AdditiveMana(int v) : AdditiveModifier(v) { id_ = "ADDITIVE_LIFE"; }
+   AdditiveMana() : AdditiveModifier() { id_ = constants::ADD_MANA; }
+   AdditiveMana(int v) : AdditiveModifier(v) { id_ = constants::ADD_MANA; }
  };
 
  struct MultiplicativeMana : MultiplicativeModifier {
-   MultiplicativeMana() : MultiplicativeModifier() { id_ = "MULTIPLICATIVE_LIFE"; }
-   MultiplicativeMana(int v) : MultiplicativeModifier(v) { id_ = "MULTIPLICATIVE_LIFE"; }
+   MultiplicativeMana() : MultiplicativeModifier() { id_ = constants::MULTI_MANA; }
+   MultiplicativeMana(int v) : MultiplicativeModifier(v) { id_ = constants::MULTI_MANA; }
  };
