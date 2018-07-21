2cd064c00ff059504c78ba83adcc4cde74105890
Kevin Zhang <kzhaaang@gmail.com>
2018-07-21 00:40:17 -0400

    #107 | assign all base attributes

--- src/entities/Character.cpp
+++ src/entities/Character.cpp
@@ -3,4 +3,17 @@
 
 Character::Character(entityx::Entity e) : ref(e) {
   ref.assign<Life>();
+  ref.assign<Mana>();
+  ref.assign<LifeRegen>();
+  ref.assign<ManaRegen>();
+  ref.assign<Armour>();
+  ref.assign<Evasion>();
+  ref.assign<Accuracy>();
+  ref.assign<Block>();
+  ref.assign<Dodge>();
+  ref.assign<FireRes>();
+  ref.assign<ColdRes>();
+  ref.assign<LightningRes>();
+  ref.assign<CritChance>();
+  ref.assign<CritMulti>();
 }
