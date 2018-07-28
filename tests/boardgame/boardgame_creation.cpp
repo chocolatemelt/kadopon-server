7a092e9965a59fd0fb7e8352cc29d6d503435917
Kevin Zhang <kzhaaang@gmail.com>
2018-07-28 15:03:40 -0400

    closes #97 | test tile and board

--- tests/boardgame/boardgame_creation.cpp
+++ tests/boardgame/boardgame_creation.cpp
@@ -10,4 +10,12 @@ TEST_CASE("creates a board", "[boardgame][board][tile]") {
   Board board("Forest", 10, 10);
 
   board.add_tile(Position(0, 0), Tile());
+  board.add_tile(Position(-1, -1), Tile());
+  board.add_tile(Position(10, 10), Tile());
+
+  CHECK(1 == board.board.size());
+
+  board.add_tile(Position(0, 1), Tile());
+
+  CHECK(2 == board.board.size());
 }
