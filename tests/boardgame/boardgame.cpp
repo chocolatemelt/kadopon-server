530afe55d54f3ec9672335367b68a369affec47b
Kevin Zhang <kzhaaang@gmail.com>
2018-08-05 17:47:11 -0400

    closes #129 | fix whitespace

--- tests/boardgame/boardgame.cpp
+++ tests/boardgame/boardgame.cpp
@@ -24,25 +24,25 @@ TEST_CASE("creates a board", "[boardgame][board][tile]") {
 }
 
 TEST_CASE("properly wraps generic map functions", "[boardgame][board]") {
-    std::string layout =
-      "001000"
-      "111111"
-      "101010"
-      "101010"
-      "111011"
-      "001000";
-    Board test(layout, 6, 6);
+  std::string layout =
+    "001000"
+    "111111"
+    "101010"
+    "101010"
+    "111011"
+    "001000";
+  Board test(layout, 6, 6);
 
-    SECTION("can check if a board has a tile at given location") {
-      CHECK(test.find(Position(0, 0)) == test.end());
-      CHECK(test.find(Position(0, 2)) != test.end());
-    }
+  SECTION("can check if a board has a tile at given location") {
+    CHECK(test.find(Position(0, 0)) == test.end());
+    CHECK(test.find(Position(0, 2)) != test.end());
+  }
 
-    SECTION("can retrieve tiles and add them with [] operator") {
-      CHECK(1 == test[Position(0, 2)].mv);
+  SECTION("can retrieve tiles and add them with [] operator") {
+    CHECK(1 == test[Position(0, 2)].mv);
 
-      CHECK(test.find(Position(0, 0)) == test.end());
-      test[Position(0, 0)] = Tile();
-      CHECK(test.find(Position(0, 0)) != test.end());
-    }
+    CHECK(test.find(Position(0, 0)) == test.end());
+    test[Position(0, 0)] = Tile();
+    CHECK(test.find(Position(0, 0)) != test.end());
+  }
 }
