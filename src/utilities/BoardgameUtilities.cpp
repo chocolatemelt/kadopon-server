bb3cfd052a41cffc5fbc57c6cf2245ab60bbdc05
Kevin Zhang <kzhaaang@gmail.com>
2018-08-05 17:08:25 -0400

    #90 | pathfinder helper

---
+++ src/utilities/BoardgameUtilities.cpp
@@ -0,0 +1,30 @@
+#include "utilities/BoardgameUtilities.hpp"
+
+static std::set<Position> BoardgameUtilities::pathfinder(Position start,
+                                                         Board board,
+                                                         int mv,
+                                                         std::set<Position> &ret) {
+  if(mv) {
+    // check neighboring tiles
+    pathfinder_helper(start, start.north(), board, mv, ret);
+    pathfinder_helper(start, start.south(), board, mv, ret);
+    pathfinder_helper(start, start.east(), board, mv, ret);
+    pathfinder_helper(start, start.west(), board, mv, ret);
+  }
+  else {
+    return ret;
+  }
+}
+
+void pathfinder_helper(Position start, Position check, Board board, int mv, std::set<Position> &ret) {
+  if(start != check) {
+    if(board.find(check) != board.end()) {
+      int cost = mv - board[check].mv;
+      if(cost) pathfinder(check, board, cost, ret);
+      else {
+        ret.insert(check)
+        pathfinder(check, board, cost, ret);
+      }
+    }
+  }
+}
---
+++ src/utilities/BoardgameUtilities.hpp
@@ -0,0 +1,25 @@
+/**
+ * Various utility functions for the boardgame.
+ */
+#pragma once
+
+#include <set>
+
+#include "boards/Board.hpp"
+#include "misc/Position.hpp"
+
+class BoardgameUtilities {
+public:
+  /**
+   * Finds the set of tiles reachable by the given amount of movement points on a board.
+   * @param  Position      start position
+   * @param  Board         board
+   * @param  int           movement points
+   * @param  set<Position> set of tiles used in recursion
+   * @return               set of tiles given as their position on the board
+   */
+  static std::set<Position> pathfinder(Position, Board, int, std::set<Position> &);
+
+private:
+  BoardgameUtilities() {}
+};
