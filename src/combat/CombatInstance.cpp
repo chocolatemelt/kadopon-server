be1105deb719afd287040ae86afae0ddb46bcc5e
gitqinhub <gitqinhub@users.noreply.github.com>
2019-02-22 20:21:15 -0500

    #140 | mockup of combat instance

---
+++ src/combat/CombatInstance.cpp
@@ -0,0 +1,46 @@
+/**
+ * Implementation of combat instance.
+ */
+#include "CombatInstance.hpp"
+/*
+ //simulate pokemon-style rpg combat
+ int main() {
+   //display menu, basic options (akin to pokemon games)
+   std::cout << "Select an action (1-4):" << std::endl;
+   //action1, basic attack
+   std::cout << "1. Action 1" << std::endl;
+  //action2, spell
+   std::cout << "2. Action 2" << std::endl;
+  //action3, item
+   std::cout << "3. Action 3" << std::endl;
+  //action 4, flee
+   std::cout << "4. Action 4" << std::endl;
+
+  unsigned int playerselection;
+
+  switch (playerselection) {
+   case 1:
+     std::cout << "You attack." << std::endl;
+     //TODO call damage instance with appropriate arguments
+     std::cout << //TODO target takes x dmg, dodges, attack misses, etc << std::endl;
+     break;
+   case 2:
+     std::cout << "You use a spell." << std::endl;
+     //TODO call damage instance with appropriate arguments
+     std::cout << //TODO target takes x dmg, dodges, spell misses, etc << std::endl;
+     break;
+   case 3:
+     std::cout << "You use an item." << std::endl;
+     //TODO call item function with appropriate arguments
+     std::cout << //TODO item does x << std::endl;
+     break;
+   case 4:
+     std::cout << "You attempt to flee." << std::endl;
+     //TODO call flee function with appropriate arguments
+     std::cout << //TODO flee successful? y/n << std::endl;
+     break;
+   default:
+     //TODO error
+   }
+ }
+*/
