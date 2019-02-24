ea38a38a3d7d045cc61fb7e94cf83fd3ec3180d0
Kevin Zhang <kzhaaang@gmail.com>
2019-02-24 18:23:55 -0500

    #161 | add generic game instance class

---
+++ src/instance/GameInstance.cpp
@@ -0,0 +1,6 @@
+#include "instance/GameInstance.hpp"
+
+GameInstance::GameInstance() {
+  // we would do some setup here...
+  // if we had any!!!
+}
---
+++ src/instance/GameInstance.hpp
@@ -0,0 +1,14 @@
+/**
+ * Defines a single instance of a Kadopon game.
+ */
+#pragma once
+
+#include "misc/Types.hpp"
+
+class GameInstance {
+public:
+  /**
+   * Creates an instance of Kadopon with default values.
+   */
+  GameInstance();
+};
