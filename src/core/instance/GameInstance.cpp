b7649abce93155eb1ea5cd4c1edf94072ef8bf2f
Kevin Zhang <kzhang@federatedwireless.com>
2019-02-25 17:27:32 -0500

    #170 | generic character registry

--- src/core/instance/GameInstance.cpp
+++ src/core/instance/GameInstance.cpp
@@ -1,12 +1,19 @@
 #include "character/CharacterFactory.hpp"
 #include "core/instance/GameInstance.hpp"
+#include "spdlog/spdlog.h"
 
 Kadopon::GameInstance::GameInstance() {
   // initialize the global registry (within game instance scope)
   Kadopon::Registry registry;
 
   // create a player
-  const Kadopon::Entity player = Character::createCharacter(registry);
+  const Kadopon::Entity player = Character::createPlayer(registry);
 
-  // presumably, things happen here
+  // presumably, things happen here...
+  // since we're still just testing, i'll just keep the game loop code here for now
+  // obviously this is temporary and should be handled separately so as not to be tightly coupled
+  spdlog::info("kadopon poc starting...");
+
+  // ... and fin
+  spdlog::info("kadopon poc closed");
 }
--- src/core/server/KadoponMain.cpp
+++ src/core/server/KadoponMain.cpp
@@ -1,8 +1,10 @@
+#include "core/instance/GameInstance.hpp"
 #include "core/server/KadoponMain.hpp"
 
 using namespace Kadopon;
 
 int KadoponMain::runner() {
   // single-threaded for now, but we'll see how we can eventually manage multiple instances
+  GameInstance main;
   return 0;
 }
