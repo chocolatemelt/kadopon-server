4fb6e504e8ccd034424fdda90f53de9b168457e0
Kevin Zhang <kzhaaang@gmail.com>
2019-02-24 19:15:43 -0500

    #161 | quick comments

--- src/instance/GameInstance.cpp
+++ src/instance/GameInstance.cpp
@@ -2,6 +2,11 @@
 #include "instance/GameInstance.hpp"
 
 GameInstance::GameInstance() {
+  // initialize the global registry (within game instance scope)
   Kadopon::Registry registry;
+
+  // create a player
   const Kadopon::Entity player = Character::createCharacter(registry);
+
+  // presumably, things happen here
 }
