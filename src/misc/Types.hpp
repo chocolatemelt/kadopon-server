3551d05fdd84aab202965971588f1cfab0423e29
Kevin Zhang <kzhang@federatedwireless.com>
2019-02-26 22:32:38 -0500

    #168 | add websocket types

--- src/misc/Types.hpp
+++ src/misc/Types.hpp
@@ -4,8 +4,12 @@
 #pragma once
 
 #include <entt/entity/registry.hpp>
+#include <ws/server_ws.hpp>
 
 namespace Kadopon {
   using Registry = entt::DefaultRegistry;
   using Entity = Registry::entity_type;
+
+  using WSServer = SimpleWeb::SocketServer<SimpleWeb::WS>;
+  using WSEndpoint = SimpleWeb::SocketServerBase<boost::asio::basic_stream_socket<boost::asio::ip::tcp> >::Endpoint;
 }
