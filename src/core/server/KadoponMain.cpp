030681c14260776e42100dbc6869924d84ef62ef
Kevin Zhang <kzhaaang@gmail.com>
2019-03-05 02:03:35 -0500

    #175 | sigint handler to only kill the server when sigint is received

--- src/core/server/KadoponMain.cpp
+++ src/core/server/KadoponMain.cpp
@@ -1,12 +1,27 @@
+#include <czmq.h>
+#include <csignal>
+#include <spdlog/spdlog.h>
+
 #include "core/api/NetworkAPI.hpp"
 #include "core/instance/GameInstance.hpp"
 #include "core/server/KadoponMain.hpp"
 
 using namespace Kadopon;
 
+static int terminate = 0;
+
+void sig_handler(int s) {
+  terminate = 1;
+  spdlog::info("caught signal {}, terminating", s);
+}
+
 int KadoponMain::runner() {
-  // single-threaded for now, but we'll see how we can eventually manage multiple instances
-  NetworkAPI network;
-  network.init();
+  // im not a bad programmer i promise
+  struct sigaction sigIntHandler;
+  sigIntHandler.sa_handler = sig_handler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+  sigaction(SIGINT, &sigIntHandler, NULL);
+  while(!terminate);
   return 0;
 }
